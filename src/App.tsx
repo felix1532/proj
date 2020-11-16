db0e472a624639f7328638f5cd4bc9ca1e105c88
felix_pc <prokop.golubtsov@gmail.com>
2020-11-16 16:50:14 +0300

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table id='table'>
+      <thead>
+        <tr>
+          <th scope='col'>Имя </th>
+          <th scope='col'>Фамилия</th>
+          <th scope='col'>Отчество</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope='col'>Голубцов </th>
+          <th scope='col'>Прокоп</th>
+          <th scope='col'>Юрьевич</th>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
